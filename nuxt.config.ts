f9fce86e026db8064cf4dbaa486ad6c7009886b9
Amir Safari <77434000+amirsafaricg@users.noreply.github.com>
2026-02-05 16:52:35 +0330

    Added Tauri

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  ssr: false,
+  modules: ["@nuxt/image", "@nuxt/icon", "@vite-pwa/nuxt"],
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      name: "My Tauri App",
+      short_name: "TauriApp",
+      theme_color: "#ffffff",
+    },
+  },
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  devServer: {
+    host: '0.0.0.0', // This allows the network to find the server
+    port: 3000
+  },
+});
---
+++ src-tauri/gen/android/build.gradle.kts
@@ -0,0 +1,26 @@
+buildscript {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        mavenCentral()
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.9.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+    }
+}
+
+allprojects {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        mavenCentral()
+        google()
+    }
+}
+
+tasks.register("clean").configure {
+    delete("build")
+}
+
